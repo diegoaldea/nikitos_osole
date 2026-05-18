55371bfb519e30a72f51059ceeea7afbb0fa3cba
diegoaldea <diegolucianoaldea@gmail.com>
2026-05-17 21:27:06 -0300

    feat: routes added

--- nikitos/routes/web.php
+++ nikitos/routes/web.php
@@ -1,33 +1,42 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\Admin\RecipeController as AdminRecipeController;
+use App\Http\Controllers\Admin\LocationController as AdminLocationController;
+use App\Http\Controllers\Admin\ContactMessageController;
+use App\Http\Controllers\Admin\SiteContentController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/productos', [ProductController::class, 'index'])->name('products.index');
+Route::get('/productos/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/recetas', [RecipeController::class, 'index'])->name('recipes.index');
+Route::get('/recetas/{recipe}', [RecipeController::class, 'show'])->name('recipes.show');
+Route::get('/donde-comprar', [LocationController::class, 'index'])->name('locations.index');
+Route::get('/contacto', [ContactController::class, 'index'])->name('contact.index');
+Route::post('/contacto', [ContactController::class, 'store'])->name('contact.store');
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', AdminProductController::class);
+    Route::resource('recipes', AdminRecipeController::class);
+    Route::resource('locations', AdminLocationController::class);
+    Route::get('messages', [ContactMessageController::class, 'index'])->name('messages.index');
+    Route::patch('messages/{message}/read', [ContactMessageController::class, 'markAsRead'])->name('messages.read');
+    Route::get('site-contents', [SiteContentController::class, 'index'])->name('site-contents.index');
+    Route::put('site-contents', [SiteContentController::class, 'update'])->name('site-contents.update');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
