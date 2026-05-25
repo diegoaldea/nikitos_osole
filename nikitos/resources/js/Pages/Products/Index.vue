5fd871ce40ae4fcc76582841f5d416d0f371ebc4
diegoaldea <diegolucianoaldea@gmail.com>
2026-05-25 16:53:31 -0300

    feat: Products page created

---
+++ nikitos/resources/js/Pages/Products/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <PublicLayout>
+        <section class="relative h-[410px]">
+            <img src="/images/img-products.png" alt="Productos" class="w-full h-full object-cover" />
+            <div class="absolute inset-0 flex items-center justify-center">
+                <h1 class="text-6xl mt-3 font-bold text-white">Productos</h1>
+            </div>
+        </section>
+
+        <section class="bg-white py-14 mt-6 pt-6">
+            <div class="mx-auto px-16">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[25px]">
+                    <CategoryCard v-for="category in categories" :key="category.id" :category="category" />
+                </div>
+            </div>
+        </section>
+    </PublicLayout>
+</template>
+
+<script setup>
+import PublicLayout from '@/Layouts/PublicLayout.vue';
+import CategoryCard from '@/Components/CategoryCard.vue';
+
+defineProps({
+    categories: Array,
+});
+</script>
