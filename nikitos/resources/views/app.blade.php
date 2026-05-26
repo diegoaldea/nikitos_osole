d86c6e4fe42658c05c90130c9db1b0072289f2d7
diegoaldea <diegolucianoaldea@gmail.com>
2026-05-26 17:56:40 -0300

    chore: tab title & favicon updated

--- nikitos/resources/views/app.blade.php
+++ nikitos/resources/views/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Nikitos [DEV]</title>
+
+        <link rel="icon" type="image/png" href="/images/logo.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
